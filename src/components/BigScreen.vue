574eb21175dbbec406c7efa82d06440443d2adcf
seuxkj <1538890100@qq.com>
2022-12-02 12:27:01 +0800

    test api success

--- src/components/BigScreen.vue
+++ src/components/BigScreen.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     数字大屏
+    <el-button @click="test()"></el-button>
   </div>
 </template>
 
 <script>
+import {getTests} from '../../api/data'
 export default {
-
+  data(){
+    return {
+      city_code:'320100'
+    }
+  },
+  methods:{
+    test(){
+      getTests(this.city_code).then((res)=>{
+        console.log(res)
+      })
+    }
+  }
 }
 </script>
 
