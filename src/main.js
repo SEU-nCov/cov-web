1a78364ccbdc267671975912984db57c93629da9
zouyingcao <57442064+zouyingcao@users.noreply.github.com>
2022-12-04 01:20:59 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import echarts from "echarts";
+import dataV from '@jiaminghi/data-view'
 
+Vue.use(dataV)
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+
+Vue.prototype.$echarts = require("echarts")
 
 new Vue({
   render: h => h(App),
